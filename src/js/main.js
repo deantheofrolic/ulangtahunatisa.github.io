2d309e1d788929904fb0e70d6fb4109c38fdc25e
mrheckel <superdean2016@gmail.com>
2021-10-08 11:25:25 +0700

    Removing delay

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
 // Delay 8 seconds, then load main page
 $(window).on("load", function(){
-     $(".loader-container").delay(8000).fadeOut("slow");
+     $(".loader-container").fadeOut("slow");
 });
 
 // Open form with button
 function openForm() {
      window.open("https://forms.gle/bisaMZHAJGX3Q7Py7", "_blank")
 }
+
+
